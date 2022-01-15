c97885d694d96a095dcb145b632842b0b98886d0
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-01-15 00:26:52 -0300

    Corrigir html

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('#botao');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('status')
+const condicao = document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt=data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML= data.status;
+    }); 
+        // fetch é uma função que faz uma chamada, um endereço tipo "fetch liga p API do rick e faça algo"
+        // `` dois acentos é onde coloca o endereço da API rick
+
+}
+
+botao.onclick = pegarPersonagem;
